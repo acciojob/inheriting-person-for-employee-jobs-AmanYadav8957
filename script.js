0b64d4c5271062ad4abdb377eb363813c5bcb7dd
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2024-12-21 23:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// complete this js code
-function Person(name, age) {}
+// Person class definition  
+function Person(name, age) {  
+    this.name = name; // Initialize name  
+    this.age = age;   // Initialize age  
+}  
 
-function Employee(name, age, jobTitle) {}
+// Method to greet  
+Person.prototype.greet = function() {  
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);  
+};  
 
-// Do not change code below this line
-window.Person = Person;
+// Employee class definition  
+function Employee(name, age, jobTitle) {  
+    // Call the Parent (Person) constructor to set name and age  
+    Person.call(this, name, age);   
+    this.jobTitle = jobTitle; // Initialize jobTitle  
+}  
+
+// Inherit from Person  
+Employee.prototype = Object.create(Person.prototype);  
+Employee.prototype.constructor = Employee; // Correct reference to Employee constructor  
+
+// Method to greet including job title  
+Employee.prototype.jobGreet = function() {  
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);  
+};  
+
+// Do not change code below this line  
+window.Person = Person;  
 window.Employee = Employee;
